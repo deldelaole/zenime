cf94012d5e69f32da76c0c0fe0ff6b2ff20ca160
deldelaole <bobopolonium@gmail.com>
2026-04-26 22:05:27 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,13 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  base: "/", // This tells the site to load from the main URL
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./"),
+      "@": path.resolve(__dirname, "./src"), // Points to your source folder
     },
   },
+  build: {
+    outDir: "dist", // This ensures the 'dist' folder we talked about is created
+  }
 })
