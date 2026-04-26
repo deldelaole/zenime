b1adcc28ec5320f3dc2ac08d8cda8fe8231b0dee
deldelaole <bobopolonium@gmail.com>
2026-04-26 22:17:17 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // This tells the computer: "@" is exactly the "src" folder inside this directory.
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      // This sets "@" to the folder ABOVE src.
+      // So "@/src/config" becomes "your-project/src/config"
+      // instead of "your-project/src/src/config"
+      "@": path.resolve(__dirname, "./"),
+    },
   },
   build: {
     outDir: 'dist',
